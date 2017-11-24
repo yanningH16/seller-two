b47b97fc876908c94cd17735a4e09b6cb84a2ae1
yanglx <1071126434@qq.com>
2017-11-24 11:11:53 +0800

    状态管理vuex

--- src/main.js
+++ src/main.js
@@ -4,11 +4,13 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
+import axios from 'axios'
 import 'element-ui/lib/theme-chalk/index.css'
 import './assets/stylus/index.styl'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI)
+Vue.prototype.$ajax = axios
 
 /* eslint-disable no-new */
 new Vue({
