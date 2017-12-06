a0985418aed098250b4663e97d373f62fdad0fea
yanglx <1071126434@qq.com>
2017-12-06 10:25:55 +0800

    1

--- src/assets/js/upload.js
+++ src/assets/js/upload.js
@@ -11,6 +11,20 @@ export const uploadPromise = new Promise((resolve, reject) => {
   })
 })
 
+// 上传图片
+export function uploadFile (data, file) {
+  let OSS = window.OSS.Wrapper
+  let name = file.lastModified + md5(file.name)
+  let client = new OSS({
+    region: 'oss-cn-hangzhou',
+    accessKeyId: data.accessKeyId,
+    accessKeySecret: data.accessKeySecret,
+    stsToken: data.securityToken,
+    bucket: 'baoyitech'
+  })
+  return client.multipartUpload(name, file)
+}
+
 // 获取上传图片大小
 export function getImgSize (file) {
   return new Promise((resolve, reject) => {
