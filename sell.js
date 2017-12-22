5112222c0d0f73e874195deb851046f77f22bce5
yanglx <1071126434@qq.com>
2017-12-22 17:43:57 +0800

    打包

--- sell.js
+++ sell.js
@@ -1,12 +1,12 @@
 var express = require('express')
 var proxy = require('http-proxy-middleware')
 var history = require('connect-history-api-fallback')
-// var config = require('./config/index')
+  // var config = require('./config/index')
 var compression = require('compression')
 var app = express()
 app.use(compression())
 app.use('/api', proxy({
-  target: 'http://182.61.29.51:8089',
+  target: 'http://182.61.24.42:8089',
   changeOrigin: true,
   pathRewrite: {
     '^/api': ''
@@ -16,9 +16,9 @@ app.use(history({
   index: './index.html'
 }))
 app.use(express.static('./dist'))
-// var port = process.env.PORT || config.build.port
-// 生产环境的端口号
-var port = 8011
+  // var port = process.env.PORT || config.build.port
+  // 生产环境的端口号
+var port = 8013
 module.exports = app.listen(port, function (err) {
   if (err) {
     console.log(err)
