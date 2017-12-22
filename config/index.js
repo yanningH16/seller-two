531998a51dfe78ce8a1a49be7126ece6ae2d07b7
yanning <y_ning26@163.com>
2017-12-22 15:19:20 +0800

    测试

--- config/index.js
+++ config/index.js
@@ -33,8 +33,9 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api': {
-        target: 'http://182.61.29.51:8089', // 测试环境
+        // target: 'http://182.61.29.51:8089', // 测试环境
         // target: 'http://182.61.24.42:8089', // 生产环境
+        target: 'http://106.14.212.154:8089',
         changeOrigin: true,
         pathRewrite: {
           '^/api': ''
