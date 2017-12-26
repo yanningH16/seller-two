f3dbf4d1ead073d0c3942331f7a21de4adf89b05
yanglx <1071126434@qq.com>
2017-12-26 11:55:45 +0800

    logo

--- src/base/navMenu/navMenu.vue
+++ src/base/navMenu/navMenu.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="menu">
     <div class="logo">
-      <img src="../../assets/image/logo.png" alt="Logo" class="img">
+      <img src="../../assets/image/logow.png" alt="Logo" class="img">
       <!-- <span style="color:#FF2933;line-height:80px;font-size:18px;">红商会</span> -->
     </div>
     <div class="router">
@@ -204,8 +204,8 @@ export default {
     text-align center
     .img
       position relative
-      width 180px
-      top 20px
+      top 10px
+      width 150px
   .router
     .routerBox
       font-size 0
